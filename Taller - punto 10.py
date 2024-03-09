60ad2aea976a1c22ab840b51925d1db9ac6d943f
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-09 16:59:21 -0500

    Add files via upload

---
+++ Taller - punto 10.py
@@ -0,0 +1,10 @@
+Distancia:float
+Distancia = float(input("Ingrese una distancia en metros: "))
+Tiempo_Luz= Distancia/299792458
+Tiempo_Sonido = Distancia/343.2
+Tiempo_Vehiculo_Comercial = Distancia/141.111
+Tiempo_Usaint_Bolt = Distancia/11.6667
+print("El tiempo que le tomaría a la luz recorrer la distancia: " + str(Tiempo_Luz) + " Seg")
+print("El tiempo que le tomaría al sonido (en el aire) recorrer la distancia: " + str(Tiempo_Sonido)  + " Seg")
+print("El tiempo que le tomaría al vehiculo comercial más veloz (SSC Tuatara) recorrer la distancia: " + str(Tiempo_Vehiculo_Comercial)  + " Seg")
+print("El tiempo que le tomaría a Bolt recorrer la distancia: " + str(Tiempo_Usaint_Bolt) + " Seg" )
