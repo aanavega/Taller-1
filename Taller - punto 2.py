9d22e1d5ca04b9d75ed0f3a67bda3eab6190a8ac
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-11 18:56:32 -0500

    Update Taller - punto 2.py

--- Taller - punto 2.py
+++ Taller - punto 2.py
@@ -1,6 +1,12 @@
-n : int
-n = int(input("Ingrese un numero real: "))
-if (n % 2) == 0 :
-    print("El numero " + str(n) + " es par")
-else:
-    print("El numero " + str(n) + " es impar")
+n:float
+c:float
+d:float
+n = input("Digite un primer numero real: ")
+c = input("Digite un segundo numero real: ")
+d = input("Digite un tercer numero real: ")
+if n>c and n>d:
+    print("El numero " + str(n) + " es el numero mayor")
+elif c>n and c>d:
+        print("El numero " + str(c) + " es el numero mayor")
+elif d>n and d>c:
+            print("El numero " + str(d) + " es el numero mayor")
