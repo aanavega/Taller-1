a8b9d19f5849e125eafc457c2da33aa4a6d8bd70
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-12 09:36:43 -0500

    Update Taller - punto 2.py

--- Taller - punto 2.py
+++ Taller - punto 2.py
@@ -4,9 +4,9 @@ tercer_numero:float
 primer_numero = input("Digite un primer numero real: ")
 segundo_numero = input("Digite un segundo numero real: ")
 tercer_numero = input("Digite un tercer numero real: ")
-if primer_numero>segundo_numero and primer_numero>tercer_numero:
+if primer_numero > segundo_numero and primer_numero > tercer_numero:
     print("El numero " + str(primer_numero) + " es el numero mayor")
-elif segundo_numero>primer_numero and segundo_numero>tercer_codigo:
+elif segundo_numero > primer_numero and segundo_numero > tercer_codigo:
         print("El numero " + str(segundo_numero) + " es el numero mayor")
-elif tercer_numero>primer_numero and tercer_numero>segundo_numero:
-            print("El numero " + str(tercer_numero) + " es el numero mayor")
+elif tercer_numero > primer_numero and tercer_numero > segundo_numero:
+          print("El numero " + str(tercer_numero) + " es el numero mayor")
