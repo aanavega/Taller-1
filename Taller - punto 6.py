3f3910425e40a1313b341aad3be57bf29c58052c
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-12 09:42:46 -0500

    Update Taller - punto 6.py

--- Taller - punto 6.py
+++ Taller - punto 6.py
@@ -1,7 +1,7 @@
 Vocales=["a","e","i","o","u"]
 Consonantes= ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
-x = input("ingrese una letra: ")
-if x in Vocales:
-    print("La letra " + str(x)+ " es una vocal")
+letra = input("ingrese una letra: ")
+if letra in Vocales:
+    print("La letra " + str(letra)+ " es una vocal")
 elif x in Consonantes:
-        print("La letra " + str(x)+ " es consonante")
+        print("La letra " + str(letra)+ " es consonante")
