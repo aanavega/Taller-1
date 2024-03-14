eac42bf58e7cca57a2586ba65fc407ec33330a47
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-13 20:54:18 -0500

    Update Taller - punto 2.py

--- Taller - punto 2.py
+++ Taller - punto 2.py
@@ -1,12 +1,15 @@
-primer_numero:float
-segundo_numero:float
-tercer_numero:float
-primer_numero = input("Digite un primer numero real: ")
-segundo_numero = input("Digite un segundo numero real: ")
-tercer_numero = input("Digite un tercer numero real: ")
-if primer_numero > segundo_numero and primer_numero > tercer_numero:
-    print("El numero " + str(primer_numero) + " es el numero mayor")
-elif segundo_numero > primer_numero and segundo_numero > tercer_numero:
-        print("El numero " + str(segundo_numero) + " es el numero mayor")
-elif tercer_numero > primer_numero and tercer_numero > segundo_numero:
-           print("El numero " + str(tercer_numero) + " es el numero mayor")
+   # Leer tres numeros reales
+     primer_numero:float
+     segundo_numero:float
+     tercer_numero:float
+     primer_numero = input("Digite un primer numero real: ")
+     segundo_numero = input("Digite un segundo numero real: ")
+     tercer_numero = input("Digite un tercer numero real: ")
+
+   # Evaluar las desigualdades de magnitud de cada numero
+     if primer_numero > segundo_numero and primer_numero > tercer_numero:
+         print("El numero " + str(primer_numero) + " es el numero mayor")
+     elif segundo_numero > primer_numero and segundo_numero > tercer_numero:
+            print("El numero " + str(segundo_numero) + " es el numero mayor")
+     elif tercer_numero > primer_numero and tercer_numero > segundo_numero:
+                print("El numero " + str(tercer_numero) + " es el numero mayor")
