594fcc1ecf20559397e6e2030dfbe13c2452b2a9
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-13 20:55:58 -0500

    Update Taller - punto 6.py

--- Taller - punto 6.py
+++ Taller - punto 6.py
@@ -1,7 +1,12 @@
-Vocales=["a","e","i","o","u"]
-Consonantes= ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+# Definir dos conjuntos con sus elementos
+vocales=["a","e","i","o","u"]
+consonantes= ["b","c","d","f","g","h","j","k","l","m","n","p","q","r","s","t","v","w","x","y","z"]
+
+# Leer una letra cualquiera
 letra = input("ingrese una letra: ")
-if letra in Vocales:
+
+# Determinar si la letra hace parte del conjunto Vocales
+if letra in vocales:
     print("La letra " + str(letra)+ " es una vocal")
-elif x in Consonantes:
+elif letra in consonantes:
         print("La letra " + str(letra)+ " es consonante")
