aa8a737878e701c78f5e88bd0ec2deebc127c13f
aanavega <159086001+aanavega@users.noreply.github.com>
2024-03-13 20:55:11 -0500

    Update Taller - punto 4.py

--- Taller - punto 4.py
+++ Taller - punto 4.py
@@ -1,7 +1,10 @@
+# Leer dos numeros reales
 primer_numero: float 
 primer_numero = float(input("Ingrese un numero real: "))
 segundo_numero: float 
 segundo_numero = float(input("Ingrese otro numero real: "))
+
+# Dterminar si el residuo del primer numero entre el segundo es 0
 if (primer_numero%segundo_numero) == 0:
     print("El numero " + str(primer_numero)+ " es multiplo de " + str(segundo_numero))
 else:
